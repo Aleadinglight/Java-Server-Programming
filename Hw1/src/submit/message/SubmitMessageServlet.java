019d38ee78b901c2395af98c7de4acee8221687b
Aleadinglight <huutamnguyen2302@gmail.com>
2018-10-08 12:00:00 +0100

    delete unecessary lines

--- Hw1/src/submit/message/SubmitMessageServlet.java
+++ Hw1/src/submit/message/SubmitMessageServlet.java
@@ -27,7 +27,6 @@ public class SubmitMessageServlet extends HttpServlet {
 		response.setContentType("text/html");
 	  
 		PrintWriter out=response.getWriter();
-		//git commit --allow-empty --date="Mon Oct 8 10:00 2018 +0100" -m "added submitmessage method"
 	  
 		out.println("<html>");
 		out.println("<body>");
